94a8364f0bdf29c264f3bfd957faea728e3ff6c7
thedcode <darygl14@gmail.com>
2018-12-20 15:06:36 -0500

    Initial Project Add

---
+++ src/main/java/guru/springframework/springframeworkpetclinic/SpringframeworkPetclinicApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.springframeworkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkPetclinicApplication.class, args);
+    }
+
+}
+
